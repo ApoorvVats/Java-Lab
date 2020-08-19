a045ab72f218153dcc1cc6d87527a5e4466314cd
Apoorv Vats <apoorvvats42@gmail.com>
2020-08-19 10:00:42 +0530

    Initial Commit

---
+++ ConvertCharactertoString.java
@@ -0,0 +1,8 @@
+public class ConvertCharactertoString
+{
+	public static void main(String[] args) {
+		char ch[]={'A','p','o','o','r','v',' ','V','a','t','s'};
+		String str=String.valueOf(ch);
+		System.out.println(str);
+	}
+}
